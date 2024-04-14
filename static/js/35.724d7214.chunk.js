db710820582cb4e7fcb4c673b64635b6e1c3eca4
SofiaProtsiv <71260387+SofiaProtsiv@users.noreply.github.com>
2024-04-14 22:27:45 +0000

    Deploying to gh-pages from @ SofiaProtsiv/vehicle-project@1c0f0d59afe354588542076a8523307f72050860 🚀

---
+++ static/js/35.724d7214.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvehicle_project=self.webpackChunkvehicle_project||[]).push([[35],{2035:function(e,n,c){c.r(n),c.d(n,{default:function(){return t}});var r=c(184),t=function(){return(0,r.jsx)("h1",{children:"The Camper Rent"})}}}]);
+//# sourceMappingURL=35.724d7214.chunk.js.map
